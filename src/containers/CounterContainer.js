4405c0c99d0da20a2edd74d48519e7ca41dc7e85
thinknormal <nornalstory@gmail.com>
2020-11-26 15:52:56 +0900

    Hooks의 useSelect를 이용한 컨테이너

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,23 +1,39 @@
-//키 커넥터
+//리액트와 리덕스 스토어간, 키 커넥터 역할을 하는 컨테이너
 
 import React from "react";
-import { connect } from "react-redux";
+//import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux"; //***<-- connect대신, Hooks의 useSelect를 이용한 컨테이너
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+// const CounterContainer = ({ number, increase, decrease }) => {
+//   return (
+//     <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+//   );
+// };
+
+// //익명함수로 바로 적용
+// export default connect(
+//   (state) => ({
+//     number: state.counter.number,
+//   }),
+//   {
+//     increase, //파라미터 함수가 아닌 객체함수로 전달
+//     decrease, //객체함수를 넣으면 connect함수가 알아서 bindActionCreators 작업을 대신 해줌
+//   }
+// )(CounterContainer); //최종목적지
+
+//*** <-- connect대신, Hooks의 useSelect를 이용한 컨테이너
+const CounterContainer = () => {
+  const number = useSelector((state) => state.counter.number);
+  const dispatch = useDispatch();
   return (
-    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+    <Counter
+      number={number}
+      onIncrease={() => dispatch(increase())}
+      onDecrease={() => dispatch(decrease())}
+    />
   );
 };
 
-//익명함수로 바로 적용
-export default connect(
-  (state) => ({
-    number: state.counter.number,
-  }),
-  {
-    increase, //파라미터 함수가 아닌 객체함수로 전달
-    decrease, //객체함수를 넣으면 connect함수가 알아서 bindActionCreators 작업을 대신 해줌
-  }
-)(CounterContainer); //최종목적지
+export default CounterContainer;
