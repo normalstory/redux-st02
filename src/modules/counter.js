174f8cf5c7c106926b1921e390a83ffbb358c960
thinknormal <nornalstory@gmail.com>
2020-11-26 15:25:37 +0900

    redux-actions 적용

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,26 +1,39 @@
+//*yarn add redux-action
+import { createAction, handleActions } from "redux-actions";
+
 //redux - Ducks pettern 01 액션타입
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
-//redux - Ducks pettern 02 액션생성함수
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+// //redux - Ducks pettern 02 액션생성함수
+// export const increase = () => ({ type: INCREASE });
+// export const decrease = () => ({ type: DECREASE });
+//redux - Ducks pettern 02 액션생성함수 + createAction
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 //redux - Ducks pettern 03 초기상태
 const initialState = {
   number: 0,
 };
 
-//redux - Ducks pettern 04 리듀서
-function counter(state = initialState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return { number: state.number + 1 };
-    case DECREASE:
-      return { number: state.number - 1 };
-    default:
-      return state;
-  }
-}
-
+// //redux - Ducks pettern 04 리듀서
+// function counter(state = initialState, action) {
+//   switch (action.type) {
+//     case INCREASE:
+//       return { number: state.number + 1 };
+//     case DECREASE:
+//       return { number: state.number - 1 };
+//     default:
+//       return state;
+//   }
+// }
+//redux - Ducks pettern 04 리듀서 + createAction
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initialState
+);
 export default counter;
--- src/modules/todos.js
+++ src/modules/todos.js
@@ -1,18 +1,33 @@
+//*yarn add redux-action
+import { createAction, handleActions } from "redux-actions";
+
 //redux - Ducks pettern 01 액션타입
 const CHANGE_INPUT = "todos/CHANGE_INPUT";
 const INSERT = "todos/INSERT";
 const TOGGLE = "todos/TOGGLE";
 const REMOVE = "todos/REMOVE";
 
-//redux - Ducks pettern 02 액션생성함수
-export const changeInput = (input) => ({ type: CHANGE_INPUT, input });
-let id = 4; //example text 개수, 새글 추가될떄마다 +1을 시작하기 위한 기준값
-export const insert = (text) => ({
-  type: INSERT,
-  todo: { id: id++, text, done: false },
-});
-export const toggle = (id) => ({ type: TOGGLE, id });
-export const remove = (id) => ({ type: REMOVE, id });
+// //redux - Ducks pettern 02 액션생성함수
+// export const changeInput = (input) => ({ type: CHANGE_INPUT, input });
+// let id = 4; //example text 개수, 새글 추가될떄마다 +1을 시작하기 위한 기준값
+// export const insert = (text) => ({
+//   type: INSERT,
+//   todo: { id: id++, text, done: false },
+// });
+// export const toggle = (id) => ({ type: TOGGLE, id });
+// export const remove = (id) => ({ type: REMOVE, id });
+
+//redux - Ducks pettern 02 액션생성함수 + createActions
+// export const changeInput = (input) => createActions({ CHANGE_INPUT, input });
+export const changeInput = createAction(CHANGE_INPUT, (input) => input);
+let id = 4;
+export const insert = createAction(INSERT, (text) => ({
+  id: id++,
+  text,
+  done: false,
+}));
+export const toggle = createAction(TOGGLE, (id) => id);
+export const remove = createAction(REMOVE, (id) => id);
 
 //redux - Ducks pettern 03 초기상태
 const initialState = {
@@ -36,35 +51,81 @@ const initialState = {
   ],
 };
 
-//redux - Ducks pettern 04 리듀서
-function todos(state = initialState, action) {
-  switch (action.type) {
-    case CHANGE_INPUT:
-      return {
-        ...state,
-        input: action.input,
-      };
-    case INSERT:
-      return {
-        ...state,
-        todos: state.todos.concat(action.todo),
-      };
-    case TOGGLE:
-      return {
-        ...state,
-        todos: state.todos.map((todo) =>
-          todo.id === action.id ? { ...todo, done: !todo.done } : todo
-        ),
-      };
-    case REMOVE:
-      return {
-        ...state,
-        todos: state.todos.filter((todo) => todo.id !== action.id),
-      };
+// //redux - Ducks pettern 04 리듀서
+// function todos(state = initialState, action) {
+//   switch (action.type) {
+//     case CHANGE_INPUT:
+//       return {
+//         ...state,
+//         input: action.input,
+//       };
+//     case INSERT:
+//       return {
+//         ...state,
+//         todos: state.todos.concat(action.todo),
+//       };
+//     case TOGGLE:
+//       return {
+//         ...state,
+//         todos: state.todos.map((todo) =>
+//           todo.id === action.id ? { ...todo, done: !todo.done } : todo
+//         ),
+//       };
+//     case REMOVE:
+//       return {
+//         ...state,
+//         todos: state.todos.filter((todo) => todo.id !== action.id),
+//       };
 
-    default:
-      return state;
-  }
-}
+//     default:
+//       return state;
+//   }
+// }
 
+////redux - Ducks pettern 04 리듀서 + createAction 01
+// const todos = handleActions(
+//   {
+//     [CHANGE_INPUT]: (state, action) => ({
+//       ...state,
+//       input: action.payload,
+//     }),
+//     [INSERT]: (state, action) => ({
+//       ...state,
+//       todos: state.todos.concat(action.payload),
+//     }),
+//     [TOGGLE]: (state, action) => ({
+//       ...state,
+//       todos: state.todos.map((todo) =>
+//         todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+//       ),
+//     }),
+//     [REMOVE]: (state, action) => ({
+//       ...state,
+//       todos: state.todos.filter((todo) => todo.id !== action.payload),
+//     }),
+//   },
+//   initialState
+// );
+
+////redux - Ducks pettern 04 리듀서 + createAction 02(payload에 이름설정하기)
+const todos = handleActions(
+  {
+    [CHANGE_INPUT]: (state, { payload: input }) => ({ ...state, input }),
+    [INSERT]: (state, { payload: todo }) => ({
+      ...state,
+      todos: state.todos.concat(todo),
+    }),
+    [TOGGLE]: (state, { payload: todos }) => ({
+      ...state,
+      todos: state.todos.map((todo) =>
+        todo.id === todos ? { ...todo, done: !todo.done } : todo
+      ),
+    }),
+    [REMOVE]: (state, { payload: id }) => ({
+      ...state,
+      todos: state.todos.filter((todo) => todo.id !== id),
+    }),
+  },
+  initialState
+);
 export default todos;
